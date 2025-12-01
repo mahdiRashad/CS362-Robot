efba43a12879fffd439b83aefbb56ce165a06d69
Mahdi Yahya <myahy3@uic.edu>
2025-11-30 22:00:48 -0600

    Update CarSensor.ino

--- CarSensor.ino
+++ CarSensor.ino
@@ -1,13 +1,103 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
-#include "pitches.h"
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
 
 /* ---------- Pin map (edit if you rewired) ---------- */
-const uint8_t l_trig  = 5,  l_echo  = 3;   // Left
-const uint8_t fl_trig = 6,  fl_echo = 7;   // Front-Left
+const uint8_t r_trig  = 5,  r_echo  = 3;   // Left
+const uint8_t fr_trig = 6,  fr_echo = 7;   // Front-Left
 const uint8_t f_trig  = 2,  f_echo  = 4;   // Front (center)
-const uint8_t fr_trig = 9,  fr_echo = 10;  // Front-Right
-const uint8_t r_trig  = 12, r_echo  = 11;  // Right
+const uint8_t fl_trig = 9,  fl_echo = 10;  // Front-Right
+const uint8_t l_trig  = 12, l_echo  = 11;  // Right
+
 
 
 const int LED_PIN = 13;
@@ -91,13 +181,13 @@ void setup() {
   strip.setBrightness(150); 
   strip.show(); 
 
-  Serial.begin(9600);            // optional debug
+  Serial.begin(4800);            // optional debug
 }
 
 /* Read all sensors in a left->right sweep every ~150–200 ms */
 void loop() {
   int milli = millis();
-  if (millis() - t >= 100) {     // adjust if you want faster/slower updates
+  if (millis() - t >= 10) {     // adjust if you want faster/slower updates
     t = millis();
 
     uint16_t L  = readDistanceCm(l_trig,  l_echo);
@@ -108,7 +198,7 @@ void loop() {
 
     dL = L; dFL = FL; dF = F; dFR = FR; dR = R;
     
-    if(dFL <= 20 || dF <= 20 || dFR <= 20){
+    if(dL <= 30 || dFL <= 30 || dF <= 30 || dFR <= 30 || dR <= 30){
       // Red if object closer than 20 cm
         int milli2 = millis();
 
